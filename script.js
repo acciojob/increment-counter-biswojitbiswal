e6866ba67b8ea4d2bcb18988f985595c4bdc7877
Biswojit Biswal <120778980+biswojitbiswal@users.noreply.github.com>
2024-10-25 11:27:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let count = 0;
+const btn = document.getElementById("incrementBtn");
+let counter = document.getElementById("counter");
+
+btn.addEventListener("click", function() {
+	alert(count);
+	count++;
+	counter.innerHtml = count;
+
+	
+})
